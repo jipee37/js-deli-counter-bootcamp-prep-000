93a4246b14cd423514df047401690035d1cea176
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-16 11:57:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function takeANumber(katzDeli, name){
 //var katzDeli = [] en 'globale'
 //takeANumber([], "Teddy") "Welcome, Teddy. You are number 1 in line."
 //takeANumber(["Teddy"], "Ada") "Welcome, Ada. You are number 2 in line."
+
+function nowServing(katzDeli){ 
+  if(katzDeli.length === 0) return "There is nobody waiting to be served!"
+  let message = 'The line is currently: '
+  for(let i=0; i< katzDeli.length ; ++i){
+    message += `${i+1}. ${katzDeli[i]}, `
+  }
+  return message
+}
