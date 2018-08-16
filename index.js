5ae16108357d830e8f7d523db337b3f89e873a1d
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-16 11:56:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function takeANumber(katzDeli, name){
+  katzDeli.push(name)
+  let message = ''
+  for(let i=0; i< katzDeli.length ; ++i){
+    message = `Welcome, ${katzDeli[i]}. You are number ${i+1} in line.`
+  }
+  return message
+}
+//var katzDeli = [] en 'globale'
+//takeANumber([], "Teddy") "Welcome, Teddy. You are number 1 in line."
+//takeANumber(["Teddy"], "Ada") "Welcome, Ada. You are number 2 in line."
+
+function 
